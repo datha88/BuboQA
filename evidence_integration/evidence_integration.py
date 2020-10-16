951294d62e37eacf84f7d935d6009f3ee46f9d22
Vishnudatha Kanjur <vishnudatha.kanjur@ims.uni-stuttgart.de>
2020-10-16 14:44:25 +0200

    F1 Score, Accuracy calculation

--- evidence_integration/evidence_integration.py
+++ evidence_integration/evidence_integration.py
@@ -69,13 +69,40 @@ def get_mid2wiki(filename):
         mid2wiki[sub] = True
     return mid2wiki
 
+def calc_f1(gold_mid,gold_rel,pred_id2answers):
+    precision = 0
+    recall = 0
+    f1 = 0
+    if len(pred_id2answers) == 0:
+        return (1, 0, 0)
+    relation_mid_found = 0
+    for i in range(len(pred_id2answers)):
+        if (pred_id2answers[i][0] == gold_mid and pred_id2answers[i][1] == gold_rel):
+            relation_mid_found = 1
+            break
+    if(relation_mid_found):
+        precision = precision + 1
+    
+    for i in range(len(pred_id2answers)):
+        if (pred_id2answers[i][0] == gold_mid and pred_id2answers[i][1] == gold_rel):
+            recall = recall + 1
+            break
+    precision = precision /len(pred_id2answers)
+    
+    recall = recall / 1
+    if (precision +recall>0 ):
+        f1 = 2* recall *precision/(recall +precision)
+   
+    return precision, recall, f1
+
 def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach, index_degrees, mid2wiki, is_heuristics, HITS_ENT, HITS_REL):
+    
     id2questions, id2goldmids = get_questions(data_path)
     id2mids = get_mids(ent_path, HITS_ENT)
     id2rels = get_rels(rel_path, HITS_REL)
     file_base_name = os.path.basename(data_path)
     fout = open(os.path.join(output_dir, file_base_name), 'w')
-
+    out_f = open(os.path.join(output_dir, 'error_analysis.txt'), 'w')
     id2answers = defaultdict(list)
     found, notfound_both, notfound_mid, notfound_rel = 0, 0, 0, 0
     retrieved, retrieved_top1, retrieved_top2, retrieved_top3 = 0, 0, 0, 0
@@ -83,6 +110,11 @@ def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach,
     lineids_found2 = []
     lineids_found3 = []
 
+    correct_answers_count = 0
+    avg_recall = 0
+    avg_precision = 0
+    avg_f1 = 0
+    count = 0
     # for every lineid
     for line_id in id2goldmids:
         if line_id not in id2mids and line_id not in id2rels:
@@ -101,28 +133,50 @@ def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach,
         truth_mid = id2goldmids[line_id]
         mids = id2mids[line_id]
         rels = id2rels[line_id]
+        
+        
+        
         if is_heuristics:
             for (mid, mid_name, mid_type, mid_score) in mids:
                 for (rel, rel_label, rel_log_score) in rels:
                     # if this (mid, rel) exists in FB
                     if rel in index_reach[mid]:
+                        '''if (line_id == 'test-1'):
+                            print(mid)
+                            print(rel)
+                            print(index_reach[mid])
+                            print(int(index_degrees[mid][0]))'''
                         rel_score = math.exp(float(rel_log_score))
                         comb_score = (float(mid_score)**0.6) * (rel_score**0.1)
                         id2answers[line_id].append((mid, rel, mid_name, mid_type, mid_score, rel_score, comb_score, int(mid2wiki[mid]), int(index_degrees[mid][0])))
+                        #correct_answers_count = correct_answers_count + 1
                     # I cannot use retrieved here because I use contain different name_type
-                    # if mid ==truth_mid and rel == truth_rel:
-                    #     retrieved += 1
+                    #if mid ==truth_mid and rel == truth_rel:
+                    #    retrieved += 1
             id2answers[line_id].sort(key=lambda t: (t[6], t[3],  t[7], t[8]), reverse=True)
         else:
             id2answers[line_id] = [(mids[0][0], rels[0][0])]
-
+        #print(id2answers[line_id])
+        
+        #id2answers[line_id] = [(mids[0][0], rels[0][0])]
         # write to file
         fout.write("{}".format(line_id))
         for answer in id2answers[line_id]:
             mid, rel, mid_name, mid_type, mid_score, rel_score, comb_score, _, _ = answer
             fout.write(" %%%% {}\t{}\t{}\t{}\t{}".format(mid, rel, mid_name, mid_score, rel_score, comb_score))
         fout.write('\n')
-
+        
+        precision, recall, f1 = calc_f1(truth_mid, truth_rel, id2answers[line_id])
+        avg_recall += recall
+        avg_precision += precision
+        avg_f1 += f1
+        count += 1
+        out_f.write(" %%%% {}\t{}\t{}\t".format(line_id, truth_mid, truth_rel, id2answers[line_id], f1))
+        for i in range(len(id2answers[line_id])):
+            out_f.write("{}\t{}\t".format(id2answers[line_id][i][0],id2answers[line_id][i][1]))
+        out_f.write('{}'.format(f1))
+        out_f.write('\n')
+        
         if len(id2answers[line_id]) >= 1 and id2answers[line_id][0][0] == truth_mid \
                 and id2answers[line_id][0][1] == truth_rel:
             retrieved_top1 += 1
@@ -138,6 +192,13 @@ def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach,
                 and id2answers[line_id][2][1] == truth_rel:
             retrieved_top3 += 1
             lineids_found3.append(line_id)
+        relation_mid_found = 0
+        for i in range(len(id2answers[line_id])):
+            if (id2answers[line_id][i][0] == truth_mid and id2answers[line_id][i][1] == truth_rel):
+                relation_mid_found = 1
+                break
+        if relation_mid_found:
+            correct_answers_count = correct_answers_count + 1
 
     print()
     print("found:              {}".format(found / len(id2goldmids) * 100.0))
@@ -145,7 +206,19 @@ def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach,
     print("retrieved at top 2: {}".format(retrieved_top2 / len(id2goldmids) * 100.0))
     print("retrieved at top 3: {}".format(retrieved_top3 / len(id2goldmids) * 100.0))
     #print("retrieved at inf:   {}".format(retrieved / len(id2goldmids) * 100.0))
+    
+    if is_heuristics:
+        print(correct_answers_count)
+        print(len(id2goldmids))
+        print("Accuracy : {}".format(correct_answers_count / len(id2goldmids) * 100.0))
+        avg_recall = float(avg_recall) / count
+        avg_precision = float(avg_precision) / count
+        avg_f1 = float(avg_f1) / count
+        print('Average Recall', avg_recall)
+        print('Average Precision', avg_precision)
+        print('Average F1', avg_f1)
     fout.close()
+    out_f.close()
     return id2answers
 
 
